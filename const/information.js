4ffd3875d5ccb20c08cbb7fd20234cc2d20312a0
dongquanthaito <dongquanthaito@gmail.com>
2023-02-21 03:02:10 +0800

    .

--- const/information.js
+++ const/information.js
@@ -1,5 +1,5 @@
 module.exports = {
-    endpoint: 'http://localhost:5000/',
+    endpoint: 'https://api.f8bet.club/',
     usernameBO: 'f80064',
     passBO: 'b8q632',
     linkBO: 'http://fgh.jdtmb.com'
