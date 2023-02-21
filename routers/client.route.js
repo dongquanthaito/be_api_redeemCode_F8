fc63a7ae6666ab70476f7342b1390ac0b39ad7e9
dongquanthaito <dongquanthaito@gmail.com>
2023-02-21 14:18:08 +0800

    .

--- routers/client.route.js
+++ routers/client.route.js
@@ -3,6 +3,6 @@ const Router = express.Router()
 const { getCodeClient, addPointClient } = require('../controllers/client.controller.js')
 
 Router.route('/get-code').post(getCodeClient)
-Router.route('/').post(addPointClient)
+Router.route('/check').post(addPointClient)
 
 module.exports = Router
